6493f35819c267586ff241d670870fae829fb4da
Shubh Goyal <goyal.shubh0711@gmail.com>
2024-04-14 18:38:54 +0530

    Final model

--- run.py
+++ run.py
@@ -3,15 +3,15 @@
 # from ddqn_lstm_agent import DDQN_Agent_LSTM
 from ddpg_agent import DDPG_Agent
 # from new_ddpg_agent import DDPG_Agent
+from agent import Agent
+from ddqn_agent import DDQN_Agent
+from ddqn_lstm_agent import DDQN_Agent_LSTM
+from ddqn_agent_vit import DDQN_Agent
 
 if __name__ == "__main__":
     # agent = Agent(useGPU=True, useDepth=True)
     # agent.train()
-    # ddqn_agent = DDQN_Agent(useDepth=False)
-    # ddqn_agent.train()
+    ddqn_agent = DDQN_Agent(useDepth=False)
+    ddqn_agent.train()
     # ddqn_agent = DDQN_Agent_LSTM(useDepth=False)
     # ddqn_agent.train()
-    ddpg_agent = DDPG_Agent(useDepth=False)
-    ddpg_agent.train()
-    # ddpg_agent = DDPG_Agent(useDepth=False)
-    # ddpg_agent.train()
